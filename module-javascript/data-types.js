a0aecc71e2d8e106e5edae30894213716273b487
Jonathan Bijl <sannoayo@gmail.com>
2023-09-07 22:43:16 -0400

    Update exponentiation and modulo operations

--- module-javascript/data-types.js
+++ module-javascript/data-types.js
@@ -1,3 +1,18 @@
 //Data Types in JavaScript - number & string
 //JS, there are 6 primitive data types: number, string, boolean, null, undefined, symbol
 
+//Advanced Operators
+
+//Exponentiation
+console.log(2 ** 5);
+// 2 * 2 * 2 * 2 * 2
+// => 32
+
+//Modulo
+console.log(4 / 2); // 4 / 2 = 2
+console.log(4 % 2); //With a remainder of 0
+console.log(7 / 2); // 7 / 2 = 3.5
+console.log(7 % 2); //With a remainder of 1
+
+console.log(8 % 2 === 0); // true, 8 is indeed a multiple of 2!
+console.log(9 % 2 === 0); // false, 9 is NOT a multiple of 2!
