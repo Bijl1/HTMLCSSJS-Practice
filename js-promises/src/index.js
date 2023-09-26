6736abb0a350135a6efe31adfe8f07ddfe8d0d80
Jonathan Bijl <sannoayo@gmail.com>
2023-09-26 17:35:16 -0400

    setTimeout Block

--- js-promises/src/index.js
+++ js-promises/src/index.js
@@ -34,16 +34,28 @@ function func1() {
   
   //Too many callbacks - Callback Hell
   function getDirections(step, callback, errorCallback) {
-    // setTimeout(() => {
+     setTimeout(() => {
       console.log( directions[step] );
       
       if (!directions[step]) errorCallback("Instructions not found.");
       else callback();
-    // }, 2000); 
+     }, 2000); 
   }
   
-  // Single callback
-  getDirections(0, ()=> {
-    getDirections(1, () => {});
-  });
+//   // Single callback
+//   getDirections(0, ()=> {
+//     getDirections(1, () => {});
+//   });
   
+// // Callbacks in sequence
+getDirections(0, () => {
+    getDirections(1, () => {
+      getDirections(2, () => {
+        getDirections(3, () => {
+          
+          console.log("You arrived at your destination!");
+          // getDirections(4, () => {}, (err) => console.log(err) ) ;
+        }, (err) => console.log(err));
+      }, (err) => console.log(err));
+    }, (err) => console.log(err));
+  }, (err) => console.log(err));
