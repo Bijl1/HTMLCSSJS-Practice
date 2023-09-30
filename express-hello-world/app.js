54827dd4ae83d3f17fbfe1ce714a43472b32a78b
Jonathan Bijl <sannoayo@gmail.com>
2023-09-30 19:47:42 -0400

    Dynamic Views

--- express-hello-world/app.js
+++ express-hello-world/app.js
@@ -1,53 +1,29 @@
 const express = require('express');
 const app = express();
-
 app.use(express.static('public'));
 
-// app.get('/', (request, response, next) => {
-//     console.log(request);
-//     response.send('<h1>Welcome!</h1>');
-// });
-
-// app.get('/cat', (request, response, next) => {
-//     response.send(`
-//       <!doctype html>
-//       <html>
-//         <head>
-//           <meta charset="utf-8">
-//           <title>Cat</title>
-//           <link rel="stylesheet" href="/stylesheets/style.css" />
-//         </head>
-//         <body>
-//           <h1>Cat</h1>
-//           <p>This is my second route</p>
-//           <img src="/images/cool-cat.jpg" />
-//         </body>
-//       </html>
-//     `);
-//   });
-
-
-//   app.get('/code', (request, response, next) => {
-//     response.send(`
-//       <!doctype html>
-//       <html>
-//         <head>
-//           <meta charset="utf-8">
-//           <title>Code</title>
-//           <link rel="stylesheet" href="/stylesheets/style.css" />
-//         </head>
-//         <body>
-//           <h1>Code</h1>
-//           <p>This is my third route</p>
-//           <img src="/images/cool-code.jpg" />
-//         </body>
-//       </html>
-//     `);
-//   });
-
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
 
 app.get('/home', (request, response, next) => response.sendFile(__dirname + '/views/home-page.html'));
 app.get('/cat', (request, response, next) => response.sendFile(__dirname + '/views/cat-page.html'));
 app.get('/code', (request, response, next) => response.sendFile(__dirname + '/views/code-page.html'));
+app.get("/test", (req, res) => res.render("test"));
+app.get("/about", (req, res) => res.render("about"))
+
+app.get("/", (req, res, next) => {
+    let data = {
+        name: "John",
+        lastName: "Rocking it!",
+        bio: "Passionate about learning, growing, and spreading positivity",
+        cities: ["Amsterdam", "Barcelona", "Berlin", "Lisbon", "Madrid", "Mexico City", "Miami", "Paris", "Sao Paulo"]
+
+    };
+    res.render("index", data);
+});
+    
+    
+
 
 app.listen(3000, () => console.log('My first app listening on port 3000'));
+
