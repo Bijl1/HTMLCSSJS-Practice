c002cafa13956f7da7fba3dc8ac82b65fad855fb
Jonathan Bijl <sannoayo@gmail.com>
2023-09-30 13:45:46 -0400

    Static files

--- express-hello-world/app.js
+++ express-hello-world/app.js
@@ -1,9 +1,48 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/home', (request, response, next) => {
     console.log(request);
     response.send('<h1>Welcome!</h1>');
 });
+
+app.get('/cat', (request, response, next) => {
+    response.send(`
+      <!doctype html>
+      <html>
+        <head>
+          <meta charset="utf-8">
+          <title>Cat</title>
+          <link rel="stylesheet" href="/stylesheets/style.css" />
+        </head>
+        <body>
+          <h1>Cat</h1>
+          <p>This is my second route</p>
+          <img src="/images/cool-cat.jpg" />
+        </body>
+      </html>
+    `);
+  });
+
+
+  app.get('/code', (request, response, next) => {
+    response.send(`
+      <!doctype html>
+      <html>
+        <head>
+          <meta charset="utf-8">
+          <title>Code</title>
+          <link rel="stylesheet" href="/stylesheets/style.css" />
+        </head>
+        <body>
+          <h1>Code</h1>
+          <p>This is my third route</p>
+          <img src="/images/cool-code.jpg" />
+        </body>
+      </html>
+    `);
+  });
 
 app.listen(3000, () => console.log('My first app listening on port 3000'));
